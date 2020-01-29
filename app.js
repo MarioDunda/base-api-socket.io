2b56ede07e57e9a74b0f252acc5f0eb4dec5dcfa
MarioDunda <mario.dunda@live.com>
2020-01-29 09:56:50 -0300

    atualizando cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require("cors");
 
 
  /* Definindo o db */
@@ -22,6 +23,7 @@ var indexRouter = require('./routes/index')({io: app.io, db});
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
